f1f5fa813e11889a2b3ce086b3acd994de5e77d9
Ian Strand <strandoutmedia@gmail.com>
2021-05-21 11:52:34 -0700

    Updates

---
+++ precache-manifest.d3cd781a56d021f47bc6eff1cb31f10f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30500e29f757af930380ae8d80c278a8",
+    "url": "/webd171-5/index.html"
+  },
+  {
+    "revision": "22a86904d23a07beeb5c",
+    "url": "/webd171-5/static/css/main.2776749d.chunk.css"
+  },
+  {
+    "revision": "cf53f5f1298370ab56d9",
+    "url": "/webd171-5/static/js/2.a9bd6a18.chunk.js"
+  },
+  {
+    "revision": "22a86904d23a07beeb5c",
+    "url": "/webd171-5/static/js/main.5621d71e.chunk.js"
+  },
+  {
+    "revision": "ee96eec38635e36e7c23",
+    "url": "/webd171-5/static/js/runtime-main.35e83e29.js"
+  }
+]);
